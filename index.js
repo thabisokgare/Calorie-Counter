33c7182c950c019be5eabfe43d2880ef7495e628
thabisokgare <thabisoryan8@gmail.com>
2024-10-13 14:32:31 +0200

    i am done

--- index.js
+++ index.js
@@ -1,23 +1,73 @@
-const calorieCounter = document.getElementById("calorie-counter");
-const budgetNumberInput = document.getElementById("budget");
-const entryDropdown = document.getElementById("entry-dropdown");
-const addEntryButton = document.getElementById("add-entry");
-const clearButton = document.getElementById("clear");
-const output = document.getElementById("output");
-
+const calorieCounter = document.getElementById('calorie-counter');
+const budgetNumberInput = document.getElementById('budget');
+const entryDropdown = document.getElementById('entry-dropdown');
+const addEntryButton = document.getElementById('add-entry');
+const clearButton = document.getElementById('clear');
+const output = document.getElementById('output');
 let isError = false;
 
-// Function to validate budget input
 function cleanInputString(str) {
     const regex = /[+-\s]/g;
-  return str.replace(regex, "");
-
- 
-    // Add your code here to process the input string based on the matched pattern
-    // For example, you can remove all occurrences of "hello" from the input string
-    // or replace them with another string, etc.
+    return str.replace(regex, '');
 }
 
-function isInvalidInput(str){
-    const regex = /[0-9]+e[0-9]+/i;
+function isInvalidInput(str) {
+    const regex = /\d+e\d+/i;
+    return str.match(regex);
 }
+
+function addEntry() {
+    const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
+    const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length + 1;
+    const HTMLString = ` <label for = "${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
+  
+  <input type="text" id="${entryDropdown.value}-${entryNumber}-name" name="budget" placeholder="Name"> 
+ <label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>
+ <input type="number" min="0" placeholder="calories" id="${entryDropdown.value}-${entryNumber}-calories">
+ `;
+    targetInputContainer.insertAdjacentHTML('beforeend', HTMLString);
+}
+
+function calculateCalories(e) {
+    e.preventDefault();
+    isError = false;
+    const breakfastNumberInputs = document.querySelectorAll('#breakfast input[type="number"]');
+    const lunchNumberInputs = document.querySelectorAll('#lunch input[type="number"]');
+    const dinnerNumberInputs = document.querySelectorAll('#dinner input[type="number"]');
+    const snacksNumberInputs = document.querySelectorAll('#snacks input[type="number"]');
+    const exerciseNumberInputs = document.querySelectorAll('#exercise input[type="number"]');
+    const breakfastCalories = getCaloriesFromInputs(breakfastNumberInputs);
+    const lunchCalories = getCaloriesFromInputs(lunchNumberInputs);
+    const dinnerCalories = getCaloriesFromInputs(dinnerNumberInputs);
+    const snacksCalories = getCaloriesFromInputs(snacksNumberInputs);
+    const exerciseCalories = getCaloriesFromInputs(exerciseNumberInputs);
+    const budgetCalories = getCaloriesFromInputs([budgetNumberInput]);
+
+    if (isError === true) {
+             
+    }
+}
+
+
+
+
+
+
+function getCaloriesFromInputs(list) {
+    let calories = 0;
+    for (const item of list) {
+        const currVal = cleanInputString(item.value);
+        const invalidInputMatch = isInvalidInput(currVal);
+        if (invalidInputMatch) {
+            alert(`Invalid Input: ${invalidInputMatch[0]}`);
+            isError = true;
+            return null;
+
+        }
+        calories += Number(currVal);
+    }
+    return calories;
+}
+
+
+addEntryButton.addEventListener('click', addEntry)
